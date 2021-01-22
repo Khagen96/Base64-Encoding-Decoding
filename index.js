f168d1c25274d3dc58b6de05489bbf7b1aed8342
Khagen Borah <khagen.1996.borah@gmail.com>
2021-01-22 12:09:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const readLineSync = require('readline-sync')
+console.clear();
+
+
+function stringEncode(str) {
+  ///
+  const base64 = Buffer.from(str).toString('base64');
+  return base64;
+}
+
+function stringDecode(url) {
+///
+  const original = Buffer.from(str, 'base64').toString('ascii');
+  return original;
+}
+const utilityOption = parseInt(readLineSync.question('Welcome to Base64 Encoder/Decoder. \nWhich utility function would you like to use?\n1. Base64 Encoder\n2. Base64 Decoder\n'));
+
+if(utilityOption===1){
+    const stringToEncode = readLineSync.question('\nPlease enter the string to be encoded\n');
+    const encodedString = stringEncode(stringToEncode);
+    console.log("\nYour encoded string is:\n"+ encodedString);
+}
+else if(utilityOption===2){
+    const stringToDecode = readLineSync.question('\nPlease enter the string to be decoded\n');
+    const decodedString = stringDecode(stringToDecode);
+    console.log("\nYour decoded string is:\n"+decodedString);
+}
+else{
+    console.log('Invalid option\n');
+}
